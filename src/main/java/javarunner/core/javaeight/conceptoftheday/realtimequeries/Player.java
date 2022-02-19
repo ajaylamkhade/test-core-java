901a6261c39b5f7917ba577e5f321006a0d7db17
a164789 <ajay.lamkhade@atos.net>
2022-02-20 01:04:05 +0530

    comparing and then comparing

---
+++ src/main/java/javarunner/core/javaeight/conceptoftheday/realtimequeries/Player.java
@@ -0,0 +1,46 @@
+package javarunner.core.javaeight.conceptoftheday.realtimequeries;
+
+public class Player {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public Player(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/javarunner/core/javaeight/conceptoftheday/realtimequeries/TestComparingThenComparing.java
@@ -0,0 +1,33 @@
+package javarunner.core.javaeight.conceptoftheday.realtimequeries;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class TestComparingThenComparing {
+    public static void main(String[] args) {
+        Supplier<List<Player>> suplierPlayerList = () -> getUnsortedEmployeeList();
+        List<Player> playerList = suplierPlayerList.get();
+
+        //sort the player list by first name ,if first name is same then sort by last name
+        List<Player> sortedPlayerList = playerList.stream().sorted(Comparator.comparing((Player p) -> p.getFirstName()).thenComparing((Player p) ->p.getLastName())).collect(Collectors.toList());
+        sortedPlayerList.forEach(s -> System.out.println(s));
+
+    }
+
+
+    private static ArrayList<Player> getUnsortedEmployeeList()
+    {
+        ArrayList<Player> list = new ArrayList<>();
+        list.add( new Player(2, "Lokesh", "Gupta") );
+        list.add( new Player(1, "Alex", "Gussin") );
+        list.add( new Player(4, "Brian", "Sux") );
+        list.add( new Player(5, "Neon", "Piper") );
+        list.add( new Player(3, "David", "Beckham") );
+        list.add( new Player(7, "Alex", "Beckham") );
+        list.add( new Player(6, "Brian", "Suxena") );
+        return list;
+    }
+}
